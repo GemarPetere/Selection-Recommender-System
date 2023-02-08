316d4dd059cf125a2d8ecb76bcb65a7f1801cf55
Kaarlo Sasiang <sasiang64@gmail.com>
2023-02-08 15:56:32 +0800

    /staging: initialized frontend folder & components

---
+++ frontEnd/assets/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Initialize Header, Body and Footer components
+$(function () {
+  $("#header").load("components/header.html");
+});
+$(function () {
+  $("#footer").load("components/footer.html");
+});
+$(function () {
+  $("app-root").load("pages/app.html");
+});
